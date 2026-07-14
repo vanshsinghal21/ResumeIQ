96f81fec36112473d6af3976bff5a1c59d4c6341
Vansh <vanshsinghal250@gmail.com>
2026-07-14 19:39:54 +0000

    Update mailer configuration

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,8 +1,9 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
